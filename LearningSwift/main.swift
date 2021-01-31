591b00aaa2580045fbeca408b2efd3087bbe2ce6
Juan Mendez <juan.mendez.3892@gmail.com>
2021-01-30 18:12:23 -0600

    05.enums

--- LearningSwift/main.swift
+++ LearningSwift/main.swift
@@ -1,45 +1,18 @@
-func showMessage() {
-    print("hello world")
+enum  Tacos {
+    // you can break them into separate lines prefixed with case, and no comma at the end
+    case CORN_TORTILLAS, FLOWER_TORTILLAS, HARD_SHELL, FLAUTA
 }
 
-showMessage()
+let myTacoType = Tacos.FLAUTA
 
-
-// method comes with named args
-// dart: void showMessageWithArgs({Int value})
-// kotlin: fun showMessageWithArgs(value: Int)
-func showMessageWithArgs(value: Int) {
-    print("hello world \(value)")
+// with raw type
+enum Burrito: String {
+    case king = "King"
+    case regular = "Regular"
+    case bowl = "Bowl"
 }
 
-showMessageWithArgs(value: 0)
+let myOwnKindBurrito = Burrito.bowl
 
-
-/*
- very interesting, return value works similar the way Kotlin uses the last line as a returning value found in lambdas
- we could had written, but is unusual
- func showMessageWithArgs(value: Int) -> Void {
- */
-func basicFunction() -> Int {
-    0
-}
-
-print("returned type \(basicFunction())")
-
-
-func returnIf(value: Int) -> Bool {
-    if(value > 0 ) {
-        return true
-    } else {
-        return false
-    }
-}
-
-print("returned type \(basicFunction())")
-
-
-func greeting(message: String = "Hello World") {
-    print("greeting -> \(message)");
-}
-
-greeting()
+print("type \(myOwnKindBurrito)")
+print("value \(myOwnKindBurrito.rawValue)")
