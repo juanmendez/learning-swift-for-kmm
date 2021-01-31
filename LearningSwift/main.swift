5a2ffbe3e498ef8c27e00223977e55ad9f28ae2f
Juan Mendez <juan.mendez.3892@gmail.com>
2021-01-30 18:17:30 -0600

    06: structs and dictionaries

--- LearningSwift/main.swift
+++ LearningSwift/main.swift
@@ -8,6 +8,10 @@ struct Song {
         "\(title) is a \(genre) song recorded in \(releaseYear) considered \(album)"
     }
 
+    func map() -> Dictionary<String, String> {
+        ["title": title, "album": album, "releaseYear": String(releaseYear), "genre": genre];
+    }
+
     // Swift doesn't come with cloning like Kotlin's data classes and Dart's finalize library
     func copy(
             title: String? = nil,
@@ -23,10 +27,6 @@ struct Song {
                 releaseYear: releaseYear ?? self.releaseYear,
                 genre: genre ?? self.genre
         )
-    }
-
-    func map() -> Dictionary<String, String> {
-        ["title":title, "directory": directory, "releaseYear": String(releaseYear), "genre": genre];
     }
 }
 
@@ -48,6 +48,31 @@ print("summary \(jungle.summary())")
 print("map \(jungle.map())");
 print("summary \(patience.summary())")
 
+let map = jungle.map()
 let paradiseCity = jungle.copy(title: "Paradise City")
 print(paradiseCity)
 
+for valueKey in map {
+    print("\(valueKey.key) \(valueKey.value)")
+}
+
+// same type like in Kotlin  () -> Bool
+/**
+ val boolReturn = { map["notFound"] == null }
+
+ assert(boolReturn.invoke())
+ */
+
+/**
+ Dart
+ final boolReturn = () { return map["notFound"] == null };
+ assert(boolReturn());
+ */
+let boolReturn = {
+    map["notFound"] == nil
+}
+assert(boolReturn())
+
+assert({
+    map["notFound"] == nil
+}())
