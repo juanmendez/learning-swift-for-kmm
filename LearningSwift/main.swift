7826632e04bcadcd35d5e79abebc805a9d41bbb1
Juan Mendez <juan.mendez.3892@gmail.com>
2021-06-05 15:23:45 -0500

    linkedin-learning

--- LearningSwift/main.swift
+++ LearningSwift/main.swift
@@ -1,6 +1,11 @@
-var a = [ 1, 2, 3, 4, 5, ]
+// here a: List<Int> is declared a: [Int] or Array<Int>
+var a: Array<Int> = [ 1, 2, 3, 4, 5, ]
 a.append(7) // wont work with a final a
 a.removeLast()
+
+a += [ 6, 7, 8, 9,]
+
+print(a)
 
 // Array<Int>
 print(type(of: a))
