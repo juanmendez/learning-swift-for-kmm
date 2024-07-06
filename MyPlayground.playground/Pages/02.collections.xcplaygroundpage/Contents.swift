979d1c1efb7a033a18419e5b157748c076e2f833
Juan Mendez <juan.mendez@tutanota.com>
2024-07-06 16:03:35 -0500

    .

--- MyPlayground.playground/Pages/02.collections.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/02.collections.xcplaygroundpage/Contents.swift
@@ -38,9 +38,14 @@ let oneDouble = [ 0.0, 0.1, 0.2 ]
 let twoDouble = [ 0.3, 0.4, 0.5 ]
 let threDouble = oneDouble + twoDouble
 
-empty.insert(contentsOf: oneDouble, at: 0)
-empty.insert(contentsOf: twoDouble, at: empty.count - 1)
-let shoppingList = [ "Eggs", "Milk"]
+empty += oneDouble
+empty += twoDouble
+
+var shoppingList = [ "Eggs", "Milk"]
+shoppingList += ["Coffee", "Beans"]
+shoppingList.append("Onions")
+print("shooping list \(shoppingList)")
+
 
 if shoppingList.contains([ "Eggs", "Milk", "Beans", "Coffee"]) {
     print("you are ready for breakfast")
@@ -48,9 +53,54 @@ if shoppingList.contains([ "Eggs", "Milk", "Beans", "Coffee"]) {
     print("you are ready for lunch")
 }
 
+for (index, value) in shoppingList.enumerated() {
+    print("Item \(index + 1) : \(value)")
+}
+
+// SET
+var letters = Set<String>()
+letters.insert("A")
+letters.insert("B")
+letters.insert("C")
+letters.insert("D")
+letters.insert("A")
+print("set is \(letters)")
+
+if let theZLetter = letters.remove("Z") {
+    print(":)")
+} else {
+    print(":(")
+}
 
 
+var airports = [ "ord": "Chicago", "mty": "Monterrey", "gua": "Guatemala city" ]
+airports["pty"] = "Panama city"
+
+if airports.isEmpty {
+    print("aiports is empty")
+} else {
+    print("airport is not empty")
+}
+
+/**
+ airports["DUB"]?.let { airportName ->
+  print("The name of the airport is $airportName).")
+ } ?: run {
+  print("That airport isn't in the airports dictionary.")
+ }
+ */
+
+if let airportName = airports["DUB"] {
+    print("The name of the airport is \(airportName).")
+} else {
+    print("That airport isn't in the airports dictionary.")
+}
 
 
+for entry in airports {
+    print("\(entry.key): \(entry.value)")
+}
 
-
+for (airportCode, airportName) in airports {
+    print("\(airportCode): \(airportName)")
+}
